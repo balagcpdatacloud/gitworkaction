63b39642221ef94d03e85f6df91772ccf2bc0feb
balagcpdatacloud <balagcpdatacloud@gmail.com>
2022-04-16 23:35:59 +0200

    fmt

--- bucket.tf
+++ bucket.tf
@@ -1,9 +1,3 @@
-# Specify the GCP Provider
-provider "google" {
-  project = var.project_id
-  region  = var.region
-}
-
 # Create a GCS Bucket
 resource "google_storage_bucket" "my_bucket" {
   name     = var.bucket_name
